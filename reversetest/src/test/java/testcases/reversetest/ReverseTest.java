60791124946d8ab53e599ddcf0129fb1aabc8dcf
Pooja1911 <pooja.h.jain@capgemini.com>
2018-04-19 11:34:08 +0530

    modified reverseString with test case

--- reversetest/src/test/java/testcases/reversetest/ReverseTest.java
+++ reversetest/src/test/java/testcases/reversetest/ReverseTest.java
@@ -2,21 +2,28 @@ package testcases.reversetest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotSame;
 
+
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class ReverseTest {
+	
+	ReverseString rs = new ReverseString();
+	
 	@Test
 	public void truereversecase()
 	{
-	 assertEquals("ih olleh",ReverseString.isReverseString("hello hi"));
+	 assertEquals("ih olleh",rs.isReverseString("hello hi"));
 	}
-    @Ignore
+   
 	@Test
-	public void falsereversecase()
+	public  void falsereversecase()
 	{
 		
-		assertNotEquals("test", "hhhh", ReverseString.isReverseString("hello hi"));
+//		assertNotEqual( "","ih olle", ReverseString.isReverseString("hello hi"));
+		assertNotSame("", "", rs.isReverseString("hello hi"));
 	}
 }
