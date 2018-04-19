72165ac562ee765775175542c87e6ed216b2353f
Pooja1911 <pooja.h.jain@capgemini.com>
2018-04-19 10:43:30 +0530

    palindrome with test case

--- palinTest/src/main/java/testcases/palinTest/Palindrome.java
+++ palinTest/src/main/java/testcases/palinTest/Palindrome.java
@@ -14,27 +14,42 @@ import java.util.Scanner;
 import org.apache.log4j.Logger;
 
 public class Palindrome {
-
+	final  static Logger LOGGER = Logger.getLogger(Palindrome.class.getName());
 	public static void main(String []args) {
 		// TODO Auto-generated method stub
-		final  Logger LOGGER = Logger.getLogger(Palindrome.class.getName());
+		
 		String value;
 
 		LOGGER.info("Enter the value");
 		final Scanner scan = new Scanner(System.in);
 		value = scan.next();
-		final StringBuilder input = new StringBuilder();
-		// appending the user given value into StringBuilder object
-		input.append(value);
-		// reversing the string
-		final StringBuilder input1 = input.reverse();
-		// checking for palindrome
-		if (value.equals(input1.toString())) {
-		LOGGER.info("Palindrome Number");
-		} else {
-			LOGGER.info("Not a palindrome");
-		}
+		 boolean bool=isPalindrome(value);
+		 if(bool==true)
+		 {
+			 LOGGER.info("Palindrome");
+		 }
+		 else
+		 {
+			 LOGGER.info("Not a Palindrome");
+		 }
 		scan.close();
 	}
-
+static boolean isPalindrome(final String palindrome)
+{
+	final StringBuilder input = new StringBuilder();
+	// appending the user given value into StringBuilder object
+	input.append(palindrome);
+	// reversing the string
+	final StringBuilder input1 = input.reverse();
+	// checking for palindrome
+	if(palindrome=="")
+	{
+		return false;
+	}
+	if (palindrome.equals(input1.toString())) {
+	return (true);
+	} else {
+		return (false);
+	}
+}
 }
--- palinTest/src/test/java/testcases/palinTest/PalinTestcase.java
+++ palinTest/src/test/java/testcases/palinTest/PalinTestcase.java
@@ -1,10 +1,38 @@
 package testcases.palinTest;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class PalinTestcase {
 	
 		
-	
+	@Test
+	public void checkfortruecase()
+	{
 		
-
-
+		assertEquals(true,Palindrome.isPalindrome("121"));
+	}
+		
+	@Test
+	public void checkforfalsecase()
+	{
+		
+		assertEquals(false,Palindrome.isPalindrome("121-"));
+	}
+	@Test
+	public void checkforemptycase()
+	{
+	
+		assertEquals(false,Palindrome.isPalindrome(""));
+	}
+	
+	@Ignore
+	@Test
+	public void checkfornullcase()
+	{
+	
+		assertEquals(false,Palindrome.isPalindrome(null));
+	}
 }
