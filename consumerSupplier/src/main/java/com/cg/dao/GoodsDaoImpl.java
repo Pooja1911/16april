79f0ca008584eff1bdf624240c37eb7081928da9
Pooja1911 <pooja.h.jain@capgemini.com>
2018-05-01 22:04:50 +0530

    consumer with junit and pmd

--- consumerSupplier/src/main/java/com/cg/dao/GoodsDaoImpl.java
+++ consumerSupplier/src/main/java/com/cg/dao/GoodsDaoImpl.java
@@ -1,13 +1,9 @@
 package com.cg.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.cg.dbUtil.DbConnection;
 import com.cg.model.Goods;
 
 public class GoodsDaoImpl implements IGoodsDao{
