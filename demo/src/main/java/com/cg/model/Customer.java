f05a0622d498ed1b35c6f34e9cb75bdb44a4edfa
Pooja1911 <pooja.h.jain@capgemini.com>
2018-05-11 20:54:14 +0530

    spring boot application with modified tasks

--- demo/src/main/java/com/cg/model/Customer.java
+++ demo/src/main/java/com/cg/model/Customer.java
@@ -10,13 +10,20 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.GeneratorType;
 
+import lombok.Data;
+import lombok.Getter;
+
+
+
 @Table(name="customer")
 @Entity
+@Data
 public class Customer {
 	@Id
 	@SequenceGenerator(name="seq11", initialValue=1, allocationSize=1)
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq11")
 	@Column(name="customer_id")
+	@Getter
 	public Long customerId;
 	@Column(name="customer_name")
 	public String customerName;
@@ -26,69 +33,7 @@ public class Customer {
 	public String paymentMode;
 	@Column(name="retailer_name")
 	public String retailerName;
-	/**
-	 * @return the customerId
-	 */
-	public long getCustomerId() {
-		return customerId;
-	}
-	/**
-	 * @param customerId the customerId to set
-	 */
-	public void setCustomerId(long customerId) {
-		this.customerId = customerId;
-	}
-	/**
-	 * @return the customerName
-	 */
-	public String getCustomerName() {
-		return customerName;
-	}
-	/**
-	 * @param customerName the customerName to set
-	 */
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-	/**
-	 * @return the customerAddress
-	 */
-	public String getCustomerAddress() {
-		return customerAddress;
-	}
-	/**
-	 * @param customerAddress the customerAddress to set
-	 */
-	public void setCustomerAddress(String customerAddress) {
-		this.customerAddress = customerAddress;
-	}
-	/**
-	 * @return the paymentMode
-	 */
-	public String getPaymentMode() {
-		return paymentMode;
-	}
-	/**
-	 * @param paymentMode the paymentMode to set
-	 */
-	public void setPaymentMode(String paymentMode) {
-		this.paymentMode = paymentMode;
-	}
-	/**
-	 * @return the retailerName
-	 */
-	public String getRetailerName() {
-		return retailerName;
-	}
-	/**
-	 * @param retailerName the retailerName to set
-	 */
-	public void setRetailerName(String retailerName) {
-		this.retailerName = retailerName;
-	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+	
 	@Override
 	public String toString() {
 		return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", customerAddress="
